9439b8257840d6e648e30964c1658504ea49f608
Victor Oliveira <victor.souza.41@hotmail.com>
2025-07-19 14:49:23 -0300

    Implementação de loop infinito que chama o método Menu() para interação contínua do usuário. Melhora a validação do ID utilizando int.TryParse. Adição do método ExibeOpcaoSelecionada(string input) para processar as opções do menu.

--- banco_framework/Program.cs
+++ banco_framework/Program.cs
@@ -9,14 +9,24 @@ internal class Program
         Console.WriteLine("Por favor, identifique-se");
         Console.WriteLine("");
         var pessoa = Identificacao();
+
+        while (true)
+        {
+            Menu();
+        }
     }
 
     static Pessoa Identificacao()
     {
+        int id;
         var pessoa = new Pessoa();
 
         Console.WriteLine("Seu número de identificação:");
-        pessoa.Id = int.Parse(Console.ReadLine());
+        while (!int.TryParse(Console.ReadLine(), out id))
+        {
+            Console.WriteLine("ID inválido. Digite um número válido:");
+        }
+        pessoa.Id = id;
 
         Console.WriteLine("Seu nome:");
         pessoa.Nome = Console.ReadLine();
@@ -26,8 +36,45 @@ internal class Program
         Console.Clear();
 
         Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
-        Console.ReadKey();
-        
+
         return pessoa;
+    }
+
+    static void Menu()
+    {
+        Console.WriteLine("1 - Depósito");
+        Console.WriteLine("2 - Saque");
+        Console.WriteLine("3 - Sair");
+        Console.WriteLine("--------------");
+        Console.WriteLine("Selecione uma opção:");
+
+        ConsoleKeyInfo keyInfo = Console.ReadKey();
+        Console.WriteLine();
+        string input = keyInfo.KeyChar.ToString();
+        ExibeOpcaoSelecionada(input);
+    }
+
+    static void ExibeOpcaoSelecionada(string input)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        switch (input)
+        {
+            case "1":
+                Console.WriteLine("Depósito");
+                break;
+            case "2":
+                Console.WriteLine("Saque");
+                break;
+            case "3":
+                Console.WriteLine("Sair");
+                Console.ResetColor();
+                Environment.Exit(0);
+                break;
+            default:
+                Console.Clear();
+                Menu();
+                break;
+        }
+        Console.ResetColor();
     }
 }
